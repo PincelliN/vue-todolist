6ae66ba0be08ec84e5cf5599cb619a0987be5148
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-26 21:29:55 +0200

    fix condiction and icon text decoration

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
- const{createApp} = Vue;
+const { createApp } = Vue;
 
-
- createApp({
-   data() {
-     return {
-       itemName: '',
-       itemValue: '',
-       ToDoList: [],
-     }
-   },
-   methods: {
-     AddItem() {
-       let item = {
-         Name: this.itemName,
-         Value: this.itemValue,
-       }
-       if (this.itemName.lenght !="" && this.itemValue !="") {
-         this.ToDoList.unshift(item);  
-          this.itemName = '';
-        this.itemValue = '';
-       }
-
-      
-     },
-     RemoveItem(index){
-        this.ToDoList.splice(index,1);
-     },
-      ChangeValue(index){
-      if (this.ToDoList[index].Value==0) {
-        this.ToDoList[index].Value = (parseInt(this.ToDoList[index].Value)) + 1;
-      } else {
-         this.ToDoList[index].Value = (parseInt(this.ToDoList[index].Value)) - 1;
+createApp({
+  data() {
+    return {
+      itemName: "",
+      itemValue: "",
+      ToDoList: [
+        {
+          Name: "Spesa",
+          Value: "0",
+        },
+        {
+          Name: "Compiti",
+          Value: "1",
+        },
+        {
+          Name: "Colazione",
+          Value: "0",
+        }
+      ],
+    };
+  },
+  methods: {
+    AddItem() {
+      let item = {
+        Name: this.itemName,
+        Value: this.itemValue,
+      };
+      if (this.itemName != "" && this.itemValue != "") {
+        this.ToDoList.unshift(item);
+        this.itemName = "";
+        this.itemValue = "";
       }
-      
-     } 
-   },
- }).mount('#container');
-
-  
+    },
+    RemoveItem(index) {
+      this.ToDoList.splice(index, 1);
+    },
+    ChangeValue(index) {
+      if (this.ToDoList[index].Value == 0) {
+        this.ToDoList[index].Value = parseInt(this.ToDoList[index].Value) + 1;
+      } else {
+        this.ToDoList[index].Value = parseInt(this.ToDoList[index].Value) - 1;
+      }
+    },
+  },
+}).mount("#container");
