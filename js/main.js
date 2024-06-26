a83698cacd2a15c0063b9aa3deb3e99e517090d3
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-26 16:28:31 +0200

    Add change value  item Value

--- js/main.js
+++ js/main.js
@@ -15,14 +15,26 @@
          Name: this.itemName,
          Value: this.itemValue,
        }
-      this.ToDoList.unshift(item),
-       this.itemName = ''
-       this.itemValue = ''
+       if (this.itemName.lenght !="" && this.itemValue !="") {
+         this.ToDoList.unshift(item);  
+          this.itemName = '';
+        this.itemValue = '';
+       }
 
+      
      },
      RemoveItem(index){
         this.ToDoList.splice(index,1);
-     }
+     },
+      ChangeValue(index){
+      if (this.ToDoList[index].Value==0) {
+        this.ToDoList[index].Value = (parseInt(this.ToDoList[index].Value)) + 1;
+      } else {
+         this.ToDoList[index].Value = (parseInt(this.ToDoList[index].Value)) - 1;
+      }
+      
+     } 
    },
  }).mount('#container');
 
+  
